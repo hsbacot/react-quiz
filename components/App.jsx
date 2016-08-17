1683b13cc2e40f25cce438a53f28973b7fdf843f
jswensen <joel.swensen@gmail.com>
2016-08-17 14:59:57 -0700

    working using combination of ES6 and 7

--- components/App.jsx
+++ components/App.jsx
@@ -1,16 +1,59 @@
 require("../node_modules/bootstrap/dist/css/bootstrap.min.css")
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import SumBox from './SumBox';
+import IncrementButton from './IncrementButton';
 
-export class App extends Component {
-	render() {
-		return (
-			<div>
-				<h1>React Quiz</h1>
-				<p>Begin Here</p>
-			</div>
-		);
-	}
+class App extends Component {
+
+	//ES7 property initializer
+	state = {
+		total: 0,
+		left: 0,
+		right: 0
+	};
+
+	//We could use ES6 constructor and lexical method binding like so:
+	// constructor() {
+  //    super();
+	// 	 this.handleIncrement = this.handleIncrement.bind(this);
+  // }
+
+	//OR we can do this ES7 combine property intialization with fat arrow preservation of context
+  handleIncrement = (event) => {
+    let currentTargetValue = this.state[event];
+
+    this.setState({
+      [event]: currentTargetValue += 1
+    }, () => this.updateTotal());
+  }
+
+  updateTotal() {
+    const { left, right } = this.state;
+    this.setState({
+      total: left + right
+    });
+  }
+
+  render() {
+    return (
+      <div className='container'>
+        <div className='row'>
+          <SumBox totalValue={this.state.total} />
+        </div>
+        <div className='row'>
+          <IncrementButton
+            componentId='left'
+            currentValue={this.state.left}
+            increment={this.handleIncrement} />
+          <IncrementButton
+            componentId='right'
+            currentValue={this.state.right}
+            increment={this.handleIncrement} />
+        </div>
+      </div>
+    );
+  }
 }
 
-ReactDOM.render(<App/>, document.querySelector("#myApp"));
+ReactDOM.render(<App />, document.querySelector("#myApp"));
