bcd79d4f98d78e063d167df191a5246117039144
jswensen <joel.swensen@gmail.com>
2016-08-17 20:33:13 -0700

    Numeric display box

---
+++ components/NumericDisplayBox.jsx
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+
+class NumericDisplayBox extends Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  //ES7 property initializer.  In ES6 we would put this outside of the class.
+  static propTypes = {
+    value: React.PropTypes.number.isRequired,
+    label: React.PropTypes.string
+  };
+
+  render() {
+    return (
+      <div className='col-md-12 text-center'>
+        <h1>{this.props.label}</h1>
+        <h2>{this.props.value}</h2>
+      </div>
+    );
+  }
+}
+
+export default NumericDisplayBox;
