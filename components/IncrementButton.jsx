f6a8c8e0e7661991de8eeeb89697fa1779896076
jswensen <joel.swensen@gmail.com>
2016-08-18 11:33:51 -0700

    cleanout unneeded currentValue

--- components/IncrementButton.jsx
+++ components/IncrementButton.jsx
@@ -9,20 +9,18 @@ class IncrementButton extends Component {
   //ES7 property initializer.  In ES6 we would put this outside of the class.
   static propTypes = {
     componentId: React.PropTypes.string.isRequired,
-    currentValue: React.PropTypes.number.isRequired,
     increment: React.PropTypes.func.isRequired
   }
 
   render() {
     return (
       <div className='col-md-6 text-center'>
-        <h1>{this.props.currentValue}</h1>
-        <h2
+        <h1
           onClick={this.props.increment.bind(
               this, this.props.componentId
           )}>
           Increment
-        </h2>
+        </h1>
       </div>
     );
   }
